3d9414cce446a7b66398c4691a4209dd56e69a17
Gabriel Pérez <juan.gabriel.ps@gmail.com>
2022-06-11 18:03:43 -0300

    feat: add first route (tasks) and json express middleware

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 import "dotenv/config";
 import express from "express";
+import tasks from "./routes/tasks.js";
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/v1/tasks", tasks);
 
 const port = process.env.PORT || 5000;
 
