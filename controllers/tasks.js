6a2d760d159bf9364a4a5eaf8a3e4548a457e44c
Gabriel Pérez <juan.gabriel.ps@gmail.com>
2022-06-11 18:05:42 -0300

    feat: create basic controller for tasks

---
+++ controllers/tasks.js
@@ -0,0 +1,21 @@
+const getAllTasks = (req, res) => {
+  res.send("all items");
+};
+
+const createTask = (req, res) => {
+  res.json(req.body);
+};
+
+const getTask = (req, res) => {
+  res.json({ id: req.params.id });
+};
+
+const updateTask = (req, res) => {
+  res.send("update one item");
+};
+
+const deleteTask = (req, res) => {
+  res.send("delete one item");
+};
+
+export { getAllTasks, createTask, getTask, updateTask, deleteTask };
