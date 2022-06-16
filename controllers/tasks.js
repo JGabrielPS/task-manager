e721621bc837a0866275193a43714718e4df9b66
Juan Gabriel Pérez <juan.gabriel.ps@gmail.com>
2022-06-16 13:03:19 -0300

    feat: add task model to controller, implement to create task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,9 +1,12 @@
+import Task from "../models/Task.js";
+
 const getAllTasks = (req, res) => {
   res.send("all items");
 };
 
-const createTask = (req, res) => {
-  res.json(req.body);
+const createTask = async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json(task);
 };
 
 const getTask = (req, res) => {
