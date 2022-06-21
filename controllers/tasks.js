c2650adfb1ecd5606c60a08b5cc3f18a8940b458
Juan Gabriel Pérez <juan.gabriel.ps@gmail.com>
2022-06-21 11:30:19 -0300

    refactor: add custom error creator and use it in get task and delete task where it is needed to create a custom error

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,5 +1,6 @@
 import Task from "../models/Task.js";
 import asyncWrapper from "../middleware/async.js";
+import { createCustomError } from "../errors/custom-error.js";
 
 const getAllTasks = asyncWrapper(async (req, res) => {
   const tasks = await Task.find({});
@@ -16,7 +17,7 @@ const createTask = asyncWrapper(async (req, res) => {
 const getTask = asyncWrapper(async (req, res) => {
   const { id: taskID } = req.params;
   const task = await Task.findOne({ _id: taskID });
-  if (!task) return res.status(404).json({ msg: `No task with id: ${taskID}` });
+  if (!task) return next(createCustomError(`No task with id: ${taskID}`, 404));
   res.status(200).json({ task });
 });
 
@@ -32,7 +33,7 @@ const updateTask = asyncWrapper(async (req, res) => {
 const deleteTask = asyncWrapper(async (req, res) => {
   const { id: taskID } = req.params;
   const task = await Task.findOneAndDelete({ _id: taskID });
-  if (!task) return res.status(404).json({ msg: `No task with id: ${taskID}` });
+  if (!task) return next(createCustomError(`No task with id: ${taskID}`, 404));
   res.status(200).json({ task: null, status: "success" });
 });
 
