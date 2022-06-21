5d0e4a3defec159727761bb36c6be20a67e1e71c
Juan Gabriel Pérez <juan.gabriel.ps@gmail.com>
2022-06-21 10:19:51 -0300

    feat: add not found middleware to all routes

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ import connectDB from "./db/connect.js";
 import "dotenv/config";
 import express from "express";
 import tasks from "./routes/tasks.js";
+import notFound from "./middleware/not-found.js";
 
 const app = express();
 
+app.use(express.static("./public"));
 app.use(express.json());
 
 app.use("/api/v1/tasks", tasks);
+app.use(notFound);
 
 const port = process.env.PORT || 5000;
 
