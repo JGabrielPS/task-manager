e05c618287481cbd5ee66717bfea75e46909ab4c
Juan Gabriel Pérez <juan.gabriel.ps@gmail.com>
2022-06-21 10:21:59 -0300

    feat: add more complete information in response for getting all tasks

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -3,7 +3,9 @@ import Task from "../models/Task.js";
 const getAllTasks = async (req, res) => {
   try {
     const tasks = await Task.find({});
-    res.status(200).json({ tasks });
+    res
+      .status(200)
+      .json({ status: "success", data: { tasks, nbHits: tasks.length } });
   } catch (error) {
     res.status(500).json({ msg: error });
   }
